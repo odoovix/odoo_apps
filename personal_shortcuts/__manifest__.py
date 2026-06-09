c3196e991b50021bbb8e3fab578cc4621c7f4d0f
odoovix <odoovix@gmail.com>
2026-06-09 15:07:09 +0530

    push

--- personal_shortcuts/__manifest__.py
+++ personal_shortcuts/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Personal Shortcuts",
-    "version": "18.0.1.0.2",
+    "version": "18.0.1.0.0",
     "category": "Productivity",
     "summary": "Personal shortcuts on the Odoo Enterprise home menu",
     "author": "Odoovix",
