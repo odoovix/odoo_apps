7e1187848ded0fc053d09eb5d7b82e12116550a0
odoovix <odoovix@gmail.com>
2026-06-10 14:29:36 +0530

    easy debug for odoo 18

---
+++ easy_debug_animated/README.rst
@@ -0,0 +1,11 @@
+Easy Debug Animated
+=================
+
+Easy Debug Animated adds a small paper-burst animation when the standard Odoo 18
+debug icon is clicked. It does not replace or move the debug menu.
+
+Usage
+=====
+
+Install the module, activate developer mode, then click the debug icon in the
+top bar.
---
+++ easy_debug_animated/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Easy Debug Animated',
+    'version': '18.0.1.0.0',
+    'category': 'Extra Tools',
+    'summary': 'Throw party paper when the Odoo debug icon is clicked',
+    'description': 'Adds a small celebration burst to the existing Odoo 18 debug menu button.',
+    'author': 'Odoovix',
+    'company': 'Odoovix',
+    'maintainer': 'Odoovix',
+    'support': 'odoovix@gmail.com',
+    'website': 'https://apps.odoo.com/apps/modules/browse?author=Odoovix',
+    'depends': ['web'],
+    'assets': {
+        'web.assets_backend': [
+            'easy_debug_animated/static/src/js/easy_debug_animated.js',
+            'easy_debug_animated/static/src/scss/easy_debug_animated.scss',
+            'easy_debug_animated/static/src/xml/easy_debug_animated.xml',
+        ],
+    },
+    'images': [
+        'static/description/banner.png',
+    ],
+    'license': 'LGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
